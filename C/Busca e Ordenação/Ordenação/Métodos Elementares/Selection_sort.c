93d8cfc42b77905d5b1076a9a73c396eb397adac
RoneyFelipe <felipeoliveira0743@gmail.com>
2021-10-02 22:23:50 -0400

    Selection sort crescente e decrescente inseridos

--- C/Busca e Ordenação/Ordenação/Métodos Elementares/Selection_sort.c
+++ C/Busca e Ordenação/Ordenação/Métodos Elementares/Selection_sort.c
@@ -1,1 +1,27 @@
 
+/* Recebe um número inteiro n >= 0 e um vetor v de números inteiros
+com n elementos e rearranja o vetor v de modo que fique crescente */
+
+/*Crescente*/
+void selection_sort_crescente(int n, int v[MAX]) {
+    int i, j, min;
+    for (i = 0; i < n - 1; i++) {
+        min = i;
+        for (j = i+1; j < n; j++)
+            if (v[j] < v[min])
+                min = j;
+        troca(&v[i], &v[min]);
+    }
+}
+
+/*Decrescente*/
+void selection_sort_decrescente(int n, int v[MAX]) {
+    int i, j, min;
+    for (i = 0; i < n - 1; i++) {
+        min = i;
+        for (j = i+1; j < n; j++)
+            if (v[j] > v[min])
+                min = j;
+        troca(&v[i], &v[min]);
+    }
+}
